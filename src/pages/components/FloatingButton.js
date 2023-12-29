1c2b5622c8947ee59de253e2f7a0f1160780671d
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2023-12-29 14:31:35 +0000

    floating button

---
+++ src/pages/components/FloatingButton.js
@@ -0,0 +1,15 @@
+// FloatingButton.js
+import React from "react";
+
+const FloatingButton = () => {
+  return (
+    <div className="fixed bottom-4 right-4">
+      {/* Your floating buttons go here */}
+      <a href="/" className="bg-gray-500 text-white py-2 px-4 rounded-full">
+        Home
+      </a>
+    </div>
+  );
+};
+
+export default FloatingButton;
