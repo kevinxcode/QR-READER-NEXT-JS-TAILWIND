d9d5419bfb3a946342bea4b2e6ddcb7b9a1f7a43
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2023-12-29 09:12:45 +0000

    updt

--- src/pages/qr-reader/index.js
+++ src/pages/qr-reader/index.js
@@ -7,26 +7,47 @@ import { QrReader } from 'react-qr-reader';
 
 export default function Myqr(){
   const [data, setData] = useState('No result');
+  const [selected, setSelected] = useState("environment");
+
+ 
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+
+
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
+    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
     return(
       <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-col items-center justify-center w-full flex-1 sm:px-20 text-center " >
-      <div className="flex-grow flex items-center justify-center" style={{minWidth: '880px'}}>
+      <div className="flex-grow flex items-center justify-center" style={{width: '100%'}}>
         <div className="container mx-auto px-4 py-8">
           
+        <select onChange={(e) => setSelected(e.target.value)}>
+            <option value={"environment"}>Back Camera</option>
+            <option value={"user"}>Front Camera</option>
+          </select>
        
       <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "300px" }}
+          />
       <p>{data}</p>
     
         
