908611ecfbb1e5853564520766bbe1b7866cf872
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2023-12-29 09:26:53 +0000

    Update 3 files

--- src/pages/qr-reader/index.js
+++ src/pages/qr-reader/index.js
@@ -14,20 +14,7 @@ export default function Myqr(){
   const [loadingScan, setLoadingScan] = useState(false);
 
 
-  const handleScan = async (scanData) => {
-    setLoadingScan(true);
-    console.log(`loaded data data`, scanData);
-    if (scanData && scanData !== "") {
-      console.log(`loaded >>>`, scanData);
-      setData(scanData);
-      setStartScan(false);
-      setLoadingScan(false);
-      // setPrecScan(scanData);
-    }
-  };
-  const handleError = (err) => {
-    console.error(err);
-  };
+
     return(
       <div className="flex flex-col min-h-screen">
       <Header />
@@ -35,17 +22,20 @@ export default function Myqr(){
       <div className="flex-grow flex items-center justify-center" style={{width: '100%'}}>
         <div className="container mx-auto px-4 py-8">
           
-        <select onChange={(e) => setSelected(e.target.value)}>
-            <option value={"environment"}>Back Camera</option>
-            <option value={"user"}>Front Camera</option>
-          </select>
-       
       <QrReader
             facingMode={selected}
             delay={1000}
-            onError={handleError}
-            onScan={handleScan}
-            // chooseDeviceId={()=>selected}
+           
+            onResult={(result, error) => {
+              if (!!result) {
+                setData(result?.text);
+              }
+    
+              if (!!error) {
+                console.info(error);
+              }
+            }}
+          
             style={{ width: "300px" }}
           />
       <p>{data}</p>
