f85aee194718fd48532c5c44ccd44790ac70b1e7
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2024-01-04 04:44:05 +0000

    Update 2 files

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import FloatingButton from "../components/FloatingButton";
 import TopBar from "../components/TopBar";
+import MyQrPage from "../profile/myQr";
 
 const Profile = () => {
   const router = useRouter();
@@ -27,13 +28,7 @@ const Profile = () => {
         </button>
       </div>
       <div className="container  mx-auto px-5 py-8  max-w-lg bg-white min-h-screen">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <MyQrPage />
       </div>
     </div>
   );
---
+++ src/pages/profile/myQr.tsx
@@ -0,0 +1,20 @@
+const myQr = () => {
+  return (
+    <div className="w-full max-w-full px-3 mt-3 border-2">
+      <div className=" flex flex-col min-w-0 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
+        <div className="p-4 mx-6 mb-0 text-center bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Link_pra_pagina_principal_da_Wikipedia-PT_em_codigo_QR_b.svg/1200px-Link_pra_pagina_principal_da_Wikipedia-PT_em_codigo_QR_b.svg.png"
+            className="object-contain hover:object-scale-down"
+          />
+        </div>
+        <div className="flex-auto p-4 pt-0 text-center">
+          <hr className="h-px my-4 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent"></hr>
+          <h5 className="mb-0">KEVIN ALNIZAR</h5>
+          <span className="leading-tight text-xs">Belong Interactive</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default myQr;
