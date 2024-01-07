1f9a9702491b3c27654bde9fd90fd44053ea3c11
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2024-01-07 11:39:05 +0000

    Update Spinner.js

--- src/pages/components/Spinner.js
+++ src/pages/components/Spinner.js
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="flex min-h-[90vh] items-center justify-center h-screen">
+    <div className="flex items-center justify-center min-h-[90vh]">
       <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin">
         <div className="w-4 h-4 bg-blue-500 rounded-full mx-auto"></div>
       </div>
